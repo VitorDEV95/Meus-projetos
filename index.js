72c0ea0d8d767b1f380324255a839deee6005e4d
VitorDEV95 <vittor.chaz.dev@gmail.com>
2023-07-25 15:25:14 -0300

    Atualizaão

---
+++ index.js
@@ -0,0 +1,48 @@
+function calculate() {
+var height = (document.getElementById("height").value)
+var weight = document.getElementById("weight").value
+console.log(height)
+console.log(weight)
+
+var imc = weight / height**2
+
+var text=""
+
+if(imc < 18.5) {
+text = "Você ta magro"
+
+} else if(imc < 24.9){
+text = "Você está normal"
+
+} else if (imc < 29.9) {
+text = "Você está com sobre peso"
+
+} else if (imc < 39.9) {
+text = "Você está com obesidade"
+
+} else if(imc > 39.9) {
+   text = "Você está com obesidade mórbida"
+}
+
+
+document.getElementById("text_area").innerText = text
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
